6264234ee07668c8e64ec705daf64a43ffb81564
Connor Cieslinski <one.crazy.miner@gmail.com>
2026-08-22 02:20:15 -0600

    Provide firmware-safe os clock shim

--- installer/loader.lua
+++ installer/loader.lua
@@ -61,6 +61,10 @@ function io.open(path, mode)
 end
 _G.io = io
 _G.PLASMAOS_BOOT_FILESYSTEM = filesystem
+local osTable = os or {}
+osTable.clock = osTable.clock or computer.uptime
+osTable.time = osTable.time or computer.uptime
+_G.os = osTable
 local packageTable = package or {path = "", loaded = {}}
 packageTable.path = root .. "/src/?.lua;" .. root .. "/src/?/init.lua;" .. (packageTable.path or "")
 _G.package = packageTable
